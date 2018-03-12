b2401f4466e319b1c45ce0aba2d43853208534f8
Paweł Kochanek <flagoon@o2.pl>
2018-03-12 23:26:54 +0100

    Cleaning up.

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoMocks, ITodo } from '../todo-mock';
 
 @Component({
   selector: 'app-todolist',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  mocks = [...ToDoMocks];
 
   ngOnInit() {
+    console.log(this.mocks);
   }
-
 }
