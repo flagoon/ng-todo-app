f18c17158b14eeed7aa07530431a80aaa0bc92af
Paweł Kochanek <flagoon@o2.pl>
2018-03-12 22:05:03 +0100

    Created mocks and interface

---
+++ src/app/todo-mock.ts
@@ -0,0 +1,13 @@
+export interface Todo {
+  id: number;
+  value: string;
+}
+
+export const ToDoMocks: Todo[] = [
+  { id: 1, value: 'Wyprowadzić psa' },
+  { id: 2, value: 'Pogłaskać kota' },
+  { id: 3, value: 'Pozbierać jajka' },
+  { id: 4, value: 'Posprzątać pokój' },
+  { id: 5, value: 'Wlać żonie' },
+  { id: 6, value: 'Pozmywać naczynia' }
+];
