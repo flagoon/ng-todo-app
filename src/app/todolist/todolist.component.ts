9da841bc7bc0e43b8beb9c451a26dc092399b228
Paweł Kochanek <flagoon@o2.pl>
2018-03-13 22:11:35 +0100

    Some logic for todolist component.

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -4,12 +4,24 @@ import { ToDoMocks, ITodo } from '../todo-mock';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.scss']
 })
 export class TodolistComponent implements OnInit {
   constructor() {}
 
+  selectedMock: ITodo;
+  isDone: boolean = false;
+  value: string = 'Done';
+
   mocks = [...ToDoMocks];
+  arraySize = this.mocks.length;
+
+  onClick(mock: ITodo): void {
+    this.value = 'bum';
+    this.isDone = true;
+    console.log(mock);
+    this.selectedMock = mock;
+  }
 
   ngOnInit() {
     console.log(this.mocks);
