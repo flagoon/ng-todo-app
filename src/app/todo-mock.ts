756d03c5ee2b38a85953d85db8da475dbd26013c
Paweł Kochanek <flagoon@o2.pl>
2018-03-14 21:34:38 +0100

    Added done status for mock todos.

--- src/app/todo-mock.ts
+++ src/app/todo-mock.ts
@@ -1,13 +1,14 @@
 export interface ITodo {
   id: number;
   value: string;
+  done: boolean;
 }
 
 export const ToDoMocks: ITodo[] = [
-  { id: 1, value: 'Wyprowadzić psa' },
-  { id: 2, value: 'Pogłaskać kota' },
-  { id: 3, value: 'Pozbierać jajka' },
-  { id: 4, value: 'Posprzątać pokój' },
-  { id: 5, value: 'Wlać żonie' },
-  { id: 6, value: 'Pozmywać naczynia' }
+  { id: 1, value: 'Wyprowadzić psa', done: false },
+  { id: 2, value: 'Pogłaskać kota', done: false },
+  { id: 3, value: 'Pozbierać jajka', done: true },
+  { id: 4, value: 'Posprzątać pokój', done: false },
+  { id: 5, value: 'Wlać żonie', done: false },
+  { id: 6, value: 'Pozmywać naczynia', done: true }
 ];
