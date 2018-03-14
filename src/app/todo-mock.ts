f5c1dd82c4ead14446a0c10fcd850ebcf16e6ecd
Paweł Kochanek <flagoon@o2.pl>
2018-03-14 22:01:49 +0100

    Change all done values to false.

--- src/app/todo-mock.ts
+++ src/app/todo-mock.ts
@@ -7,8 +7,8 @@ export interface ITodo {
 export const ToDoMocks: ITodo[] = [
   { id: 1, value: 'Wyprowadzić psa', done: false },
   { id: 2, value: 'Pogłaskać kota', done: false },
-  { id: 3, value: 'Pozbierać jajka', done: true },
+  { id: 3, value: 'Pozbierać jajka', done: false },
   { id: 4, value: 'Posprzątać pokój', done: false },
   { id: 5, value: 'Wlać żonie', done: false },
-  { id: 6, value: 'Pozmywać naczynia', done: true }
+  { id: 6, value: 'Pozmywać naczynia', done: false }
 ];
