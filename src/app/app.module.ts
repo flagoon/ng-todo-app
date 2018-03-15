1fbbcd8c226f57b0cb590db6eb330bf67de5b089
Paweł Kochanek <flagoon@o2.pl>
2018-03-15 21:43:14 +0100

    Added imports for pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { ShortStringPipe } from './todolist/short-string.pipe';
 
 @NgModule({
-  declarations: [AppComponent, TodolistComponent],
+  declarations: [AppComponent, TodolistComponent, ShortStringPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
