291cf333f3074facd6683f70a3548690e797dfb4
Paweł Kochanek <flagoon@o2.pl>
2018-03-15 21:42:31 +0100

    Created pipe for long names

---
+++ src/app/todolist/short-string.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shortString'
+})
+export class ShortStringPipe implements PipeTransform {
+  transform(value: string): string {
+    if (value.length > 25) {
+      return `${value.slice(0, 25)} ...`;
+    }
+    return value;
+  }
+}
