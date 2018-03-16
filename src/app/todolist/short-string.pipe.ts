831884d2c87b1361720dcf744ed1de200ecba4de
Paweł Kochanek <flagoon@o2.pl>
2018-03-16 13:28:55 +0100

    Removed spacebar.

--- src/app/todolist/short-string.pipe.ts
+++ src/app/todolist/short-string.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShortStringPipe implements PipeTransform {
   transform(value: string): string {
     if (value.length > 25) {
-      return `${value.slice(0, 25)} ...`;
+      return `${value.slice(0, 25)}...`;
     }
     return value;
   }
